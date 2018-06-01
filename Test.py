de79d2945a94594965419b5cea5b7b55d81de10f
Unknown <leothemusicghost@outlook.com>
2018-06-01 14:00:40 +0200

    Added some documentation

--- Test.py
+++ Test.py
@@ -3,19 +3,19 @@ import random
 wordlist, features = [], []
 while True:
     message = input(": ")
-    words = message.split(" ")
+    words = message.split(" ")  # Take up the message and divide it into individual words
 
-    def find_index(word_list, word):
-        for i in range(0, len(word_list)):
-            if word_list[i] == word:
-                return i
+    def find_index(word_list, word):  # Find the index of a word in a wordlist
+        for i in range(0, len(word_list)):  # Loop through every index in the wordlist
+            if word_list[i] == word:  # If the item is the same as the word,
+                return i  # return the index
 
-    def word_to_list(list_, feature_list, word_list):
+    def word_to_list(list_, feature_list, word_list):  # Return a list without duplicates and an int version.
         output = []
-        for i in range(0, len(word_list)): # For every entry in
-            if word_list[i] not in list_ and word_list[i] != '':
-                list_.append(word_list[i])
-                feature_list.append([])
+        for i in range(0, len(word_list)):  # For every entry in the list of words
+            if word_list[i] not in list_ and word_list[i] != '':  # If the entry being looped isn't in the word list,
+                list_.append(word_list[i])  # Append that to the non-duplicate list
+                feature_list.append([])  # Append it to the feature list.
                 feature_list[len(feature_list)-1].append(find_index(list_, word_list[i]))
                 feature_list[len(feature_list)-1].append(i)
         output.append(list_)
